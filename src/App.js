b9b4ba83527efc3deea38ef3b7a2fcf398ae6976
Alberto <albertoyanesjr3@hotmail.com>
2019-12-19 13:41:37 -0300

    Init config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import IntegrateSDK from "./lib/factom-harmony-integrate-js-sdk/dist/factom-harmony-integrate-js-sdk";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
